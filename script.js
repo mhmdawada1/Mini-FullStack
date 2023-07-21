aad4969174abbcec4d5f10d94fa5f211d06fa4f4
Mohammad Awada <awada@Mohammads-MacBook-Air.local>
2023-07-21 04:02:41 +0300

    fetching from sign in API

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ document.addEventListener("DOMContentLoaded", function() {
       .catch((error) => console.log(error));
     });
 })
+
+document.getElementById("signin").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    const formData = new FormData(event.target);
+    fetch("http://127.0.0.1/Register-Login-system-back-and-front-end/apis/signin.php", {
+      method: "POST",
+      body: formData
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status == "logged in") {
+        window.location.href = `dashboard.html?username=${encodeURIComponent(data.username)}`;
+    } else {
+        alert(data.status);
+      }
+    })
+    .catch((error) => console.log(error));
+  });
+  
