2808c67d6687ff6f16dae2dc4dd72a8f66b0157b
Mohammad Awada <awada@Mohammads-MacBook-Air.local>
2023-07-21 03:48:38 +0300

    creating CRUD sign up process

---
+++ signup.php
@@ -0,0 +1,33 @@
+<?php
+include('connection.php');
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+
+$check_username = $mysqli->prepare('SELECT username FROM users WHERE username=?');
+$check_username->bind_param('s', $username);
+$check_username->execute();
+$check_username->store_result();
+$username_exists = $check_username->num_rows();
+
+if ($username_exists == 0) {
+    if (strlen($password) >= 8) {
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+        $query = $mysqli->prepare('INSERT INTO users(username, password, first_name, last_name) VALUES(?,?,?,?)');
+        $query->bind_param('ssss', $username, $hashed_password, $first_name, $last_name);
+        $query->execute();
+
+        $response['status'] = "success";
+        $response['message'] = "user added";
+    } else {
+        $response['status'] = "failed";
+        $response['message'] = "Password must be at least 8 characters long.";
+    }
+} else {
+    $response['status'] = "failed";
+    $response['message'] = "Username already exists.";
+}
+
+echo json_encode($response);
