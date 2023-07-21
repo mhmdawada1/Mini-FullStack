90ba8010a1432bde2bc13810b4d9478ec12666ed
Mohammad Awada <awada@Mohammads-MacBook-Air.local>
2023-07-21 03:50:59 +0300

    adding header to avoid CORP error

--- connection.php
+++ connection.php
@@ -1,9 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
-header('Content-Type: application/json');
+
 $mysqli = new mysqli("localhost","root",null, "foodex_db");
 if (!$mysqli) {
     die('a connection was unsuccesful');
--- signin.php
+++ signin.php
@@ -1,4 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
+header('Content-Type: application/json');
 include('connection.php');
 
 $username = $_POST['username'];
